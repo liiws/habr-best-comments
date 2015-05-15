f8cf089724aec9aa29e9e325375d45a156f28df5
liiws <none@none>
2015-05-15 20:56:06 +0300

    [+] /article/*

--- habr-best-comments.meta.js
+++ habr-best-comments.meta.js
@@ -3,11 +3,13 @@
 // @namespace   http://habrahabr.ru
 // @include     http://habrahabr.ru/post/*
 // @include     http://habrahabr.ru/company/*
+// @include     http://habrahabr.ru/article/*
 // @include     http://geektimes.ru/post/*
 // @include     http://geektimes.ru/company/*
+// @include     http://geektimes.ru/article/*
 // @grant       none
 // @run-at      document-start
-// @version     0.1.5
+// @version     0.1.6
 // @downloadURL https://bitbucket.org/liiws/habr-best-comments/downloads/habr-best-comments.user.js
 // @updateURL   https://bitbucket.org/liiws/habr-best-comments/downloads/habr-best-comments.meta.js
 // ==/UserScript==
