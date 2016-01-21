fc0973c13629bc84ed63b02fb0092c51429b40d2
liiws <none@none>
2016-01-21 20:34:25 +0300

    [+] HTTPS urls (now they are in RSS)

--- habr-best-comments.meta.js
+++ habr-best-comments.meta.js
@@ -7,9 +7,15 @@
 // @include     http://geektimes.ru/post/*
 // @include     http://geektimes.ru/company/*
 // @include     http://geektimes.ru/article/*
+// @include     https://habrahabr.ru/post/*
+// @include     https://habrahabr.ru/company/*
+// @include     https://habrahabr.ru/article/*
+// @include     https://geektimes.ru/post/*
+// @include     https://geektimes.ru/company/*
+// @include     https://geektimes.ru/article/*
 // @grant       none
 // @run-at      document-start
-// @version     0.1.13
+// @version     0.1.14
 // @downloadURL https://bitbucket.org/liiws/habr-best-comments/downloads/habr-best-comments.user.js
 // @updateURL   https://bitbucket.org/liiws/habr-best-comments/downloads/habr-best-comments.meta.js
 // ==/UserScript==
