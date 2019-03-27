6fc98e6d2574462f7f27c0198a49af496deb5a10
liiws <none@none>
2019-03-27 20:55:10 +0300

    [+] news url

--- habr-best-comments.meta.js
+++ habr-best-comments.meta.js
@@ -4,12 +4,14 @@
 // @include     https://habr.com/ru/post/*
 // @include     https://habr.com/ru/company/*
 // @include     https://habr.com/ru/article/*
+// @include     https://habr.com/ru/news/*
 // @include     https://habr.com/en/post/*
 // @include     https://habr.com/en/company/*
 // @include     https://habr.com/en/article/*
+// @include     https://habr.com/en/news/*
 // @grant       none
 // @run-at      document-start
-// @version     0.4.4
+// @version     0.4.5
 // @downloadURL https://bitbucket.org/liiws/habr-best-comments/downloads/habr-best-comments.user.js
 // @updateURL   https://bitbucket.org/liiws/habr-best-comments/downloads/habr-best-comments.meta.js
 // ==/UserScript==
